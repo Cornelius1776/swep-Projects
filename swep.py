1a4a1ca461b870dabfb6425e57183641a71ed4f9
Cornelius Mamman <corneliusisaac62@gmail.com>
2024-11-14 16:19:15 +0100

    Add files via upload

--- swep.py
+++ swep.py
@@ -10,86 +10,100 @@ class Bank:
         self.account_number = ''
         self.gender = ''
         self.balance = 0
-        self.pin = 0
+        self.pin = ''
         
     # Set Pin
     def set_pin(self):
         while True:
-            pin = input('Set your pin(Must be 4 digits): ')
+            pin = input('Set your pin (Must be 4 digits): ')
             if len(pin) == 4 and pin.isdigit():
                 self.pin = pin
-                print('Congratulations! your pin has been set')
+                print('Congratulations! Your pin has been set')
                 return self.pin
             else:
                 print('Your pin should contain 4 digits and numbers only') 
     
     # Open account with the bank
-    def open_account(self):
+    @classmethod
+    def open_account(cls):
         print('Please provide your Personal details to open an account.\n')
-        self.first_name = input("Enter your first name: ")
-        self.middle_name = input("Enter your middle name: ")
-        self.surname = input("Enter your Surname: ")
-        self.gender = input("Male or Female: ")
-        self.account_number = ''.join(str(random.randint(0, 9)) for _ in range(1, 10))
-        self.pin = self.set_pin()
-        self.accounts.append(self) # appends the account object 
-        print(f'Hi {self.first_name}. Account created successfully\nYour account number is {self.account_number}') 
+        account = Bank()  # Create a new Bank instance for each new account
+        account.first_name = input("Enter your first name: ")
+        account.middle_name = input("Enter your middle name: ")
+        account.surname = input("Enter your surname: ")
+        account.gender = input("Male or Female: ")
+        account.account_number = ''.join(str(random.randint(0, 9)) for _ in range(10))
+        account.set_pin()
+        cls.accounts.append(account)  # Append the new account to the class-level accounts list
+        print(f'Hi {account.first_name}. Account created successfully.\nYour account number is {account.account_number}') 
         
     # Deposit money
-    def deposit(self):
+    @classmethod
+    def deposit(cls):
         acct_no = input("Enter the account number you want to deposit into: ")
-        for account in Bank.accounts:
+        for account in cls.accounts:
             if account.account_number == acct_no:
-                amount = int(input('How much do you want to deposit? '))
-                self.balance += amount
-                print(f'Credit alert: ₦{amount:,} deposited into your account.\nBalance: ₦{self.balance:,}')
+                try:
+                    amount = int(input('How much do you want to deposit? '))
+                    if amount > 0:
+                        account.balance += amount
+                        print(f'Credit alert: ₦{amount:,} deposited into your account.\nBalance: ₦{account.balance:,}')
+                    else:
+                        print("Amount must be greater than 0")
+                except ValueError:
+                    print("Please enter a valid amount.")
                 return
-        
         print(f'{acct_no} does not exist!')
             
-        
     # Check Balance
-    def check_balance(self):
+    @classmethod
+    def check_balance(cls):
+        acct_no = input("Enter your account number to check balance: ")
         pin = input('Enter your pin to check account balance: ')
-        for pins in Bank.accounts:
-            if pins.pin == pin:
-                print(f'Hi {self.first_name}, Your balance is ₦{self.balance:,}')
+        for account in cls.accounts:
+            if account.account_number == acct_no and account.pin == pin:
+                print(f'Hi {account.first_name}, Your balance is ₦{account.balance:,}')
                 return
+        print('Account number or pin is incorrect!')
         
-        print('Wrong pin!')
-        
-    # full Account details
-    def details(self):
-        pin = input("enter your pin: ")
-        if pin == self.pin:
-            print(f'First name: {self.first_name}\nMiddle name: {self.middle_name}\nSurname: {self.surname}\nGender: {self.gender}')
-            print(f'Account Number: {self.account_number}\nBalance: ₦{self.balance:,}\n.')
-        else:
-            print('Incorrect pin')
+    # Full Account details
+    @classmethod
+    def details(cls):
+        acct_no = input("Enter your account number: ")
+        pin = input("Enter your pin: ")
+        for account in cls.accounts:
+            if account.account_number == acct_no and account.pin == pin:
+                print(f'First name: {account.first_name}\nMiddle name: {account.middle_name}\nSurname: {account.surname}\nGender: {account.gender}')
+                print(f'Account Number: {account.account_number}\nBalance: ₦{account.balance:,}\n')
+                return
+        print('Incorrect account number or pin')
         
     # Using the program
-    def open(self):
+    @classmethod
+    def open(cls):
         print('Welcome to SWEP bank\n')
         while True:
             print('1. Open Account\n2. Deposit\n3. Check Balance\n4. View account\n5. Close Program')
-            choice = int(input("Choose action: "))
-            if choice == 1:
-                self.open_account()
-            elif choice == 2:
-                self.deposit()
-            elif choice == 3:
-                self.check_balance()
-            elif choice == 4:
-                self.details()
-            elif choice == 5:
-                print('Thanks for banking with us.')
-                break
-            else:
-                print('Invalid option! Try again')
-    
-    
-user_1 = Bank()
-user_1.open()
+            try:
+                choice = int(input("Choose action: "))
+                if choice == 1:
+                    cls.open_account()
+                elif choice == 2:
+                    cls.deposit()
+                elif choice == 3:
+                    cls.check_balance()
+                elif choice == 4:
+                    cls.details()
+                elif choice == 5:
+                    print('Thanks for banking with us.')
+                    break
+                else:
+                    print('Invalid option! Try again')
+            except ValueError:
+                print("Please enter a valid choice.")
+
+# Run the banking program
+Bank.open()
+
         
-    
         
